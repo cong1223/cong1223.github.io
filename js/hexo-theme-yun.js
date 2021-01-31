7be91af9ba8c512ed444c5a76f1208d0c04001ac
王聪 <goastwang0131@gmail.com>
2021-01-31 14:57:58 +0800

    Site updated: 2021-01-31 14:57:58

--- js/hexo-theme-yun.js
+++ js/hexo-theme-yun.js
@@ -1,6 +1,6 @@
 // global Yun
 console.log(
-  `%c ☁️ hexo-theme-yun ${CONFIG.version} %c https://github.com/YunYouJun/hexo-theme-yun`,
+  `%c ☁️ 小聪忙求内推 %c 13757154855`,
   "color: white; background: #0078E7; padding:5px 0;",
   "padding:4px;border:1px solid #0078E7;"
 );
